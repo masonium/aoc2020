e20ea989bd05a7b42fa8d052789f393b2e915910
Mason Smith <masonium@gmail.com>
2020-12-06 01:34:24 -0800

    finish day 6

---
+++ day06.jl
@@ -0,0 +1,40 @@
+f = open("C:\\Users\\Mason Smith\\Documents\\day06input.txt")
+g = readlines(f)
+
+
+function parse_groups(lines, init, red)
+    total = 0
+    curr = init
+    for line in lines
+        if line == ""
+            total += length(curr)
+            curr = init
+            continue
+        end
+        curr = red(curr, Set([c for c in line]))
+    end
+
+    total += length(curr)
+    total
+end
+
+println(parse_groups(g, Set(), union))
+println(parse_groups(g, Set([c for c in "qwertyuiopasdfghjklzxcvbnm"]), intersect))
+
+
+# ----------------
+# Second version
+# ----------------
+f = open("C:\\Users\\Mason Smith\\Documents\\day06input.txt")
+h = read(f, String)
+
+groups = Array(split(h, "\n\n"))
+println(groups)
+
+function parse_group(group, init, redf)
+    sets = map(x -> Set([c for c in x]), split(group, "\n"))
+    length(reduce(redf, sets; init=init))
+end
+
+println(sum(map(x -> parse_group(x, Set(), union), groups)))
+println(sum(map(x -> parse_group(x, Set([c for c in "qwertyuiopasdfghjklzxcvbnm"]), intersect), groups)))
